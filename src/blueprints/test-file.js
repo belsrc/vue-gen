314d0fa39b3986f71fda5e3f31b006c160d39257
Bryan Kizer <brykizer@gmail.com>
2018-12-17 23:25:10 -0500

    feat(test-template): change test template to use Jest and expand examples

--- src/blueprints/test-file.js
+++ src/blueprints/test-file.js
@@ -1,10 +1,39 @@
 const genTest = (componentName, compFile) => `import 'babel-core/register';
-import test from 'ava';
-import ${ componentName.slice(0, 1).toLowerCase() }${ componentName.slice(1) } from './${ compFile }';
+import { shallowMount } from '@vue/test-utils';
+import ${ componentName } from './${ compFile }';
 
-test('throws not yet implemented', t => {
-  t.throws(() => {
-    throw new Error('Not Yet Implemented');
+// Template uses Jest and Vue test utils as an example
+// npm i -D jest @vue/test-utils
+// But your free to use whatever test setup you prefer
+
+describe('${ componentName }', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    jest.resetModules();
+    jest.clearAllMocks();
+    wrapper = shallowMount(${ componentName });
+  });
+
+  describe('Data', () => {
+    // test('has correct <data-name> data property', () => {
+    //   expect(wrapper.vm.<data-name>).toEqual(<value>);
+    // });
+    test.skip('NOT IMPLEMENTED', () => {});
+  });
+
+  describe.skip('Computed', () => {
+    // test('has correct <computed-name> computed property', () => {
+    //   expect(wrapper.vm.<computed-name>).toEqual(<value>);
+    // });
+    test.skip('NOT IMPLEMENTED', () => {});
+  });
+
+  describe.skip('Rendering', () => {
+    // test('has the expected html structure', () => {
+    //   expect(wrapper.element).toMatchSnapshot();
+    // });
+    test.skip('NOT IMPLEMENTED', () => {});
   });
 });
 `;
