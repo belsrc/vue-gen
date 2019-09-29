29292dfc817e69bcc300238c792f22186bb99e38
Bryan Kizer <brykizer@gmail.com>
2019-09-29 02:10:52 -0400

    style: prettier + eslint

--- src/get-content.js
+++ src/get-content.js
@@ -5,7 +5,11 @@ const vueFnFile = require('./blueprints/vue-func-file');
 
 const genContent = (fileType, componentName, className, isPage, hasState, functional) =>
   fileType === '.jsx' ?
-    functional ? jsxFnFile(componentName, className) : jsxFile(componentName, className, isPage, hasState) :
-    functional ? vueFnFile(componentName, className) : vueFile(componentName, className, isPage, hasState);
+    functional ?
+      jsxFnFile(componentName, className) :
+      jsxFile(componentName, className, isPage, hasState) :
+    functional ?
+      vueFnFile(componentName, className) :
+      vueFile(componentName, className, isPage, hasState);
 
 module.exports = genContent;
