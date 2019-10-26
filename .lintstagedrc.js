97eb61b59cc2a635ac19e45c62367f54cf466c64
Bryan Kizer <brykizer@gmail.com>
2019-10-26 16:22:50 -0400

    refactor: change configs to support TS

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,8 +1,9 @@
 module.exports = {
   '*.{js,jsx,ts,tsx}': [
-    'prettier --config .prettierrc.js --write',
-    'eslint -c .eslintrc.js --fix --ignore-path .eslintignore',
-    'cross-env NODE_ENV=test jest --findRelatedTests --passWithNoTests',
+    'npx prettier --config .prettierrc.js --write',
+    'npx eslint -c .eslintrc.js --fix --ignore-path .eslintignore',
+    'npx tsc',
+    'npx cross-env NODE_ENV=test jest --findRelatedTests --passWithNoTests',
     'git add',
   ],
   '*.{md,html,json}': ['prettier --config .prettierrc.js --write', 'git add'],
