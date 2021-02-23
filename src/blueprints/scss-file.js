519a53e70c9415eae4f187b2b5efd2a61f933556
Bryan Kizer <bryan@bryanckizer.com>
2021-02-23 17:08:30 -0500

    refactor: update scss blueprint for some additional breakpoints

--- src/blueprints/scss-file.js
+++ src/blueprints/scss-file.js
@@ -2,16 +2,34 @@ const genScss = className => `// Mobile / Baseline
 .${ className } {}
 
 // Tablets
-@media only screen and (min-width: 768px) {}
+@media only screen and (min-width: 768px) {
+  .${ className } {}
+}
 
 // Hi-Res Tablets
-@media only screen and (min-width: 1024px) {}
+@media only screen and (min-width: 1024px) {
+  .${ className } {}
+}
 
 // Desktops
-@media only screen and (min-width: 1200px) {}
+@media only screen and (min-width: 1200px) {
+  .${ className } {}
+}
+
+// Mid DPI Laptop/Desktop
+@media only screen and (min-width: 1360px) {
+  .${ className } {}
+}
+
+// Hi DPI Desktop
+@media only screen and (min-width: 1440px) {
+  .${ className } {}
+}
 
 // Larger Desktops
-@media only screen and (min-width: 1920px) {}
+@media only screen and (min-width: 1920px) {
+  .${ className } {}
+}
 `;
 
 module.exports = genScss;
